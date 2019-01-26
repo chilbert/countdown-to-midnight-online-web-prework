564d14e67a7082bede0412d6f99a32054ba7f75c
Chase Hilbert <chase@heyspacetime.com>
2019-01-26 21:00:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(y)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(y)
+  while y > 0
+    puts "#{y} SECOND(S)!"
+    y -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end

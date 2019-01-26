a313c61a77fae09966aad42a9dbb87100273bf25
Chase Hilbert <chase@heyspacetime.com>
2019-01-26 21:01:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(y)
+  while y > 0
+    puts "#{y} SECOND(S)!"
+    y -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(y)
+  while y > 0
+    puts "#{y} SECOND(S)!"
+    y -= 1
+    sleep(5)
+  end
+  return "HAPPY NEW YEAR!"
 end
